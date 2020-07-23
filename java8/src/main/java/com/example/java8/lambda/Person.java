e757005cb750e129bcf0b299a466f5fe6a16b510
lisongyan123 <842166100@qq.com>
2020-07-24 00:19:51 +0800

    java8

---
+++ java8/src/main/java/com/example/java8/lambda/Person.java
@@ -0,0 +1,11 @@
+public class Person {
+    public String firstName;
+    public String lastName;
+
+    public Person() {}
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
---
+++ java8/src/main/java/com/example/java8/stream/Optionals.java
@@ -0,0 +1,77 @@
+package com.example.java8.stream;
+
+import java.util.Optional;
+import java.util.function.Supplier;
+
+public class Optionals {
+
+    static class Outer {
+        Nested nested = new Nested();
+
+        public Nested getNested() {
+            return nested;
+        }
+    }
+
+    static class Nested {
+        Inner inner = new Inner();
+
+        public Inner getInner() {
+            return inner;
+        }
+    }
+
+    static class Inner {
+        String foo = "boo";
+
+        public String getFoo() {
+            return foo;
+        }
+    }
+
+    public static void main(String[] args) {
+        test1();
+        test2();
+        test3();
+        test4();
+    }
+
+    public static <T> Optional<T> resolve(Supplier<T> resolver) {
+        try {
+            T result = resolver.get();
+            return Optional.ofNullable(result);
+        } catch (NullPointerException e) {
+            return Optional.empty();
+        }
+    }
+
+    private static void test3() {
+        Outer outer = new Outer();
+        resolve(() -> outer.getNested().getInner().getFoo())
+                .ifPresent(System.out::println);
+    }
+
+    private static void test2() {
+        Optional.of(new Outer())
+                .map(Outer::getNested)
+                .map(Nested::getInner)
+                .map(Inner::getFoo)
+                .ifPresent(System.out::println);
+    }
+
+    private static void test1() {
+        Optional.of(new Outer())
+                .flatMap(o -> Optional.ofNullable(o.nested))
+                .flatMap(n -> Optional.ofNullable(n.inner))
+                .flatMap(i -> Optional.ofNullable(i.foo))
+                .ifPresent(System.out::println);
+    }
+
+    private static void test4() {
+        Optional<String> optional = Optional.of("bam");
+        optional.isPresent();           // true
+        optional.get();                 // "bam"
+        optional.orElse("fallback");    // "bam"
+        optional.ifPresent((s) -> System.out.println(s.charAt(0)));     // "b"
+    }
+}
