112a7a3a8ea9e5ee661b9ca7447516ea74703367
lisongyan123 <842166100@qq.com>
2020-09-11 00:18:12 +0800

    算法94

--- java8/src/main/java/com/example/java8/extra/algorithm/AlgorithmMedium.java
+++ java8/src/main/java/com/example/java8/extra/algorithm/AlgorithmMedium.java
@@ -957,4 +957,31 @@ public class AlgorithmMedium {
             temp.remove(temp.size() - 1);
         }
     }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode curr = root;
+        while (curr != null || !stack.isEmpty()) {
+            while (curr != null) {
+                stack.push(curr);
+                curr = curr.left;
+            }
+            curr = stack.pop();
+            res.add(curr.val);
+            curr = curr.right;
+        }
+        return res;
+    }
 }
