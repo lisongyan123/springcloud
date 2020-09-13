b01e5c8c1665177ae9f34561a2995f45c24a6233
lisongyan123 <842166100@qq.com>
2020-09-13 23:41:48 +0800

    算法102

--- java8/src/main/java/com/example/java8/extra/algorithm/AlgorithmMedium.java
+++ java8/src/main/java/com/example/java8/extra/algorithm/AlgorithmMedium.java
@@ -1145,5 +1145,35 @@ public class AlgorithmMedium {
         dfs(node.right,list);
     }
 
+    //层次遍历
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        if(root==null) {
+            return new ArrayList<List<Integer>>();
+        }
 
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        LinkedList<TreeNode> queue = new LinkedList<TreeNode>();
+        //将根节点放入队列中，然后不断遍历队列
+        queue.add(root);
+        while(queue.size()>0) {
+            //获取当前队列的长度，这个长度相当于 当前这一层的节点个数
+            int size = queue.size();
+            ArrayList<Integer> tmp = new ArrayList<Integer>();
+            //将队列中的元素都拿出来(也就是获取这一层的节点)，放到临时list中
+            //如果节点的左/右子树不为空，也放入队列中
+            for(int i=0;i<size;++i) {
+                TreeNode t = queue.remove();
+                tmp.add(t.val);
+                if(t.left!=null) {
+                    queue.add(t.left);
+                }
+                if(t.right!=null) {
+                    queue.add(t.right);
+                }
+            }
+            //将临时list加入最终返回结果中
+            res.add(tmp);
+        }
+        return res;
+    }
 }
